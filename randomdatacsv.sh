cd0c73130c1bb5996434289b0f5f572b986a0ced
JaimeADalton <jaimeadalton@gmail.com>
2020-02-18 02:09:18 +0100

    Update randomdatacsv.sh

--- randomdatacsv.sh
+++ randomdatacsv.sh
@@ -4,7 +4,7 @@ aCadenaIndex=($(shuf -i 1-1000000 -n 1000000))
 
 function table_1 {
 	for ((j=1;j<=1000000;j++));do
-		echo "$j;${aCadenaUnica[$j]};${aCadenaIndex[$j]};${aCadenaIndex[$j]};$(($j % 100 + 1))"
+		echo "${aCadenaUnica[$j]};${aCadenaIndex[$j]};${aCadenaIndex[$j]};$(($j % 100 + 1))"
 	done > tablas.csv
 }
 
